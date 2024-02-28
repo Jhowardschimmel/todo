f538dfc24034243ff98aa29c61c8704082290dad
Eric Griffin <ejgriffin13@gmail.com>
2024-02-27 19:40:25 -0500

    first commit

--- routes/todo.js
+++ routes/todo.js
@@ -1,11 +1,13 @@
-const express = require('express'); //import express
-const router  = express.Router();
-const todoController = require('../controllers/todo');
+const express = require("express"); //import express
+const router = express.Router();
+const todoController = require("../controllers/todo");
 
-router.get('/todo', todoController.getAllTodos);
-router.post('/todo', todoController.newTodo); 
-router.delete('/todo', todoController.deleteAllTodos);
+router.get("/todo", todoController.getAllTodos);
+router.post("/todo", todoController.newTodo);
+router.delete("/todo", todoController.deleteAllTodos);
 
-router.get('/todo/:name', todoController.getOneTodo);
+router.get("/todo/:name", todoController.getOneTodo);
 //:TODO Implement routes for post and delete of single entities
 module.exports = router; // export to use in server.js
+
+//This is a change.

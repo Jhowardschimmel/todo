55fdfffdfa9c89361813f6e5d6f72068312a2db9
Eric Griffin <ejgriffin13@gmail.com>
2024-02-27 21:30:34 -0500

    feat: added router.patch function to routes

--- routes/todo.js
+++ routes/todo.js
@@ -4,11 +4,11 @@ const todoController = require("../controllers/todo");
 
 router.get("/todo", todoController.getAllTodos);
 router.post("/todo", todoController.newTodo);
-router.put("/todo", todoController.updateTodo); // I am not sure about this.
+router.patch("/todo", todoController.updateTodo); // I am not sure about this.
 router.delete("/todo", todoController.deleteAllTodos);
 
 router.get("/todo/:name", todoController.getOneTodo);
 //:TODO Implement routes for post and delete of single entities
 module.exports = router; // export to use in server.js
 
-// I forgot most of this stuff.
+// I forgot most of this stuff...

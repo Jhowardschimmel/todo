8a3364f1285e32c1877046f15d210ebc63e5d994
unknown <jhowardschimmel@gmail.com>
2024-03-02 02:58:33 -0500

    chore: getting routes set up

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,10 +1,32 @@
 // newTodo function for post todo route
+// https://curl.se/docs/httpscripting.html
 // curl -X POST http://localhost:3000/todo
-const newTodo = (req, res, next) => {
-    res.json({message: "POST new todo"}); // stub function for now
+// curl http://localhost:3000/todo
+const createTodo = (req, res, next) => {
+    res.json({ message: "POST new todo" }); // stub function for now
 };
+
+const listTodos = (req, res, next) => {
+    res.json({ message: "List todos" }); // stub function for now
+};
+
+const getTodo = (req, res, nex) => {
+    res.json({ message: "Get todo by id" })
+
+};
+
+const deleteTodo = (req, res, next) => {
+    res.json({ message: "Delete todo by id" })
+};
+
+
 
 //TODO: Stub dummy functions like aboce for routes.
 
 //TODO: Dont forget to export below.
-module.exports = {newTodo};
+module.exports = {
+    createTodo,
+    listTodos,
+    getTodo,
+    deleteTodo
+};
--- routes/todo.js
+++ routes/todo.js
@@ -2,12 +2,13 @@ const express = require("express"); //import express
 const router = express.Router();
 const todoController = require("../controllers/todo");
 
-router.get("/todo", todoController.getAllTodos);
-router.post("/todo", todoController.newTodo);
-router.patch("/todo", todoController.updateTodo); // I am not sure about this.
-router.delete("/todo", todoController.deleteAllTodos);
+router.get("/todo", todoController.listTodos);
+router.post("/todo", todoController.createTodo);
+// router.patch("/todo", todoController.updateTodo); // I am not sure about this.
+// router.delete("/todo", todoController.deleteAllTodos);
 
-router.get("/todo/:name", todoController.getOneTodo);
+router.get("/todo/:id", todoController.getTodo);
+router.delete("todo/:id", todoController.deleteTodo);
 //:TODO Implement routes for post and delete of single entities
 module.exports = router; // export to use in server.js
 

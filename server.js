e21955ea32ee9dd9ff6d960845728462dc404eff
Eric Griffin <ejgriffin13@gmail.com>
2024-04-26 13:32:08 -0400

    fix: added .env file with PORT and DB_PASSWORD variables.

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const express = require("express");
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 dotenv.config();
-const routes = require('./routes/todo');
+const routes = require("./routes/todo");
 
 const app = express();
 const PORT = process.env.PORT;
-console.log(`Your port is ${process.env.PORT}`);
+console.log(`Your port is ${PORT}`);
 
 app.use(express.json());
 // http://localhost:3000/
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", PORT);
-  });
+  console.log("Server Listening on PORT:", PORT);
+});
 
-  console.log('process.env', process.env.DB_PASSWORD)
+console.log("process.env", process.env.DB_PASSWORD);
 // create a new MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: "localhost",
+  user: "ericgriffin",
   password: process.env.DB_PASSWORD,
-  database: 'todo'
+  database: "todo",
 });
 // connect to the MySQL database
 connection.connect((error) => {
   if (error) {
-    console.error('Error connecting to MySQL database:', error);
+    console.error("Error connecting to MySQL database:", error);
   } else {
-    console.log('Connected to MySQL database!');
+    console.log("Connected to MySQL database!");
   }
 });
 // close the MySQL connection
